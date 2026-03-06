f29d2b064b99d0f2749c61303b85e56eb7f981b9
lmp2000 <lmrpais@gmail.com>
2026-03-06 22:20:29 +0000

    commit

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,21 @@
+import sys
+
+def main() -> None:
+    print("=== Command Quest ===")
+
+    argc = len(sys.argv)
+
+    if argc <= 1:
+        print("No arguments provided!")
+        print(f"Program name: {sys.argv[0]}")
+    elif argc > 1:
+        print(f"Program name: {sys.argv[0]}")
+        print(f"Arguments received: {argc - 1}")
+        for i, string in enumerate(sys.argv[1:], start=1):
+            print(f"Argument {i}: {string}")
+
+    print(f"Total arguments: {argc}")
+
+
+if __name__ == "__main__":
+    main()
